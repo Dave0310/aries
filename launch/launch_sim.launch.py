36c1ea5dee7b13d105acb68813e64ebb66c495d8
Dave0310 <dpuertas@my.centennialcollege.ca>
2025-01-03 19:47:18 -0500

    Gazebo 2

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,5 +1,4 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
@@ -8,22 +7,24 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    package_name = 'articubot_one'  # <--- CHANGE ME
+    # Include the robot_state_publisher launch file
+    package_name = 'articubot_one'  # Update with your package name
 
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
-        )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+            )]), 
+        launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
     joystick = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'joystick.launch.py'
-        )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory(package_name), 'launch', 'joystick.launch.py'
+            )]), 
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
@@ -34,21 +35,25 @@ def generate_launch_description():
         remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
     )
 
-    # Gazebo Fortress launch file
+    # Gazebo Fortress Launch
     gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gz_sim_ros2'), 'launch', 'gz_sim.launch.py')]),
-        launch_arguments={'gz_args': '-s libgazebo_ros_factory.so -s libgazebo_ros_init.so'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('gz_ros2_control'), 'launch', 'gz_sim.launch.py'
+            )]),
+        launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
     )
 
-    # Run the spawner node for Gazebo Fortress
-    spawn_entity = Node(package='gz_ros2_control', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot',
-                                   '-allow_renaming', 'true'],
-                        output='screen')
+    # Spawn robot into Gazebo Fortress
+    spawn_entity = Node(
+        package='gz_ros2_control', 
+        executable='create',
+        arguments=['-topic', 'robot_description', '-name', 'my_bot'],
+        output='screen'
+    )
 
+    # Controller and Joint Spawners
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
@@ -61,7 +66,6 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
-    # Launch them all!
     return LaunchDescription([
         rsp,
         joystick,
